00c601f25eaef9ccd3672f883febf37e497dbae1
Mykyta H. <flori00993@gmail.com>
2026-02-08 11:01:18 -0500

    Chapter two! # Learn about Jinja templates with some logic and inheritance structure for easier scalability # Mock objects (like users in routes.py) are placeholders for things that are not implemented yet, but shoudn't slow down development

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mykyta'}
+    posts = [
+        {
+            'author': {'username': 'Miguel'},
+            'body': 'Beauriful day in Montreal!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Shrek movie was so cool!'    
+        },
+        {
+            'author' : {'username': 'Megan'},
+            'body': 'Coding is not for the weak 🥀'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
