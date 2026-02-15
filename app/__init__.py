05f0e7c63641d495d6fa12bec178cadda70a06fe
Mykyta H. <flori00993@gmail.com>
2026-02-15 14:22:08 -0500

    Chapter three! # Introdution to Flask-WTF extension which is a wrapper for forms with security features (like one against CRSF) # Made a class for configurations which may contain app configuration and other subclasses for any other configuration (if necessary) # User login form renders with some validation rules and feedback ! Although the app is very simple and doesn't have _yet_ the infrastructure to log in users, there's a temporary message that confirms the reception of user form on the server side

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from .config import Config # for uncertain reason the interpreter cannot find the file, so using a relative import
 
 app = Flask(__name__) # app here is the actual Flask application instance
+app.config.from_object(Config)
 
 from app import routes
 
